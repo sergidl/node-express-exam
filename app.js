92a06ce7fea27529245b69c64cde22c559ae55db
rgarciamvm <91730364+rgarciamvm@users.noreply.github.com>
2022-03-07 16:36:11 +0100

    Add files via upload

---
+++ app.js
@@ -0,0 +1,23 @@
+import express from 'express';
+import moviesRouter from './routers/moviesRouter.js'
+import clientErrorHandler from './middleware/errorHandler.js';
+import errorRouter from './routers/errorRouter.js';
+
+const app=express();
+
+app.use(express.json());
+
+app.use((req,res,next)=>{
+    console.log('---->app.js');
+    next();
+});
+
+app.use('/movies',moviesRouter);
+//Otros direccionaminetos ...
+
+
+app.use('*',errorRouter);
+app.use(clientErrorHandler);
+
+
+export default app;
---
+++ routers/errorRouter.js
@@ -0,0 +1,7 @@
+import Router from 'express';
+import HttpError from 'http-errors';
+const router = Router();
+
+router.all('/',(req,res,next)=>next(HttpError(404,{message:"no existe la ruta"})));
+
+export default router;
